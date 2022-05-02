6eb1fbf3f8cebbc3e7587a5042ac58e8999f1545
Franco Valledor <franco.valledor@tailorsoft.co>
2022-05-02 02:36:24 -0300

    Updates

---
+++ precache-manifest.7992b654bf755b693b8277496be4275f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39982e821985aff9784c28bbae71a507",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d1c46cc628559fd9eb7",
+    "url": "/portfolio/static/css/3.7331fdf5.chunk.css"
+  },
+  {
+    "revision": "436813431fc92e2617be",
+    "url": "/portfolio/static/css/4.94917f11.chunk.css"
+  },
+  {
+    "revision": "bbeba8ae1ff963320e2b",
+    "url": "/portfolio/static/css/main.b7acc99b.chunk.css"
+  },
+  {
+    "revision": "e700d59276736dc1c8f0",
+    "url": "/portfolio/static/js/2.e85f9fa3.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.e85f9fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1c46cc628559fd9eb7",
+    "url": "/portfolio/static/js/3.10324136.chunk.js"
+  },
+  {
+    "revision": "436813431fc92e2617be",
+    "url": "/portfolio/static/js/4.afe8fb20.chunk.js"
+  },
+  {
+    "revision": "bbeba8ae1ff963320e2b",
+    "url": "/portfolio/static/js/main.be1dd76f.chunk.js"
+  },
+  {
+    "revision": "bad08f64c0dfb8d48994",
+    "url": "/portfolio/static/js/runtime-main.5948abe5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "8bea63143a25a4d3c22e47cfb7af0fc8",
+    "url": "/portfolio/static/media/FCyT.8bea6314.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5bfcbabd8351c5a62ccfe0ebe532b39b",
+    "url": "/portfolio/static/media/logomuni-min.5bfcbabd.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "7796b3d070443b522b2ba41080da27e9",
+    "url": "/portfolio/static/media/tailorsoft.7796b3d0.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "4f3f3ae40c8dcd7211d2a92ee8c2b375",
+    "url": "/portfolio/static/media/tbs.4f3f3ae4.png"
+  }
+]);
